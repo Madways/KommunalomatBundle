72ba47429d3cd0636155b2a3eb7f2cf24bdb8178
morbidick <lukas.mohrbacher@web.de>
2014-04-15 03:20:11 +0200

    selected answer to questions by users get now saved

--- Controller/QuestionController.php
+++ Controller/QuestionController.php
@@ -7,14 +7,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Madways\KommunalomatBundle\Entity\Question as Question;
+use Madways\KommunalomatBundle\Entity\UserAnswer as UserAnswer;
+use Madways\KommunalomatBundle\Entity\User as User;
 
 class QuestionController extends Controller
 {
     /**
     * @Template()
     */
-    public function indexAction($weight)
+    public function indexAction($weight, Request $request) 
     {
+        $last_question = false;
+
+        $user = new User(); //TODO: should be done per sessions
+        $user->setSession("abc");
 
         // Initiate Repository
         $repository = $this->getDoctrine()
@@ -24,12 +30,50 @@ class QuestionController extends Controller
         $question = $repository->findOneByWeight($weight);
 
         if (!$question) {
-            throw $this->createNotFoundException(
-                'No question found for weight '.$weight
-            );
+            $question = new Question();
+            $last_question = true;
+        }
+
+        // empty Object
+        $answer = new UserAnswer();
+
+
+        // Create the Form
+        $form = $this->createFormBuilder($answer)
+                ->setAction($this->generateUrl("MadwaysKommunalomatBundleQuestion", array('weight' => $weight+1)))
+                ->add('question', 'hidden', array('mapped' => false, 'data' => $question->getID() ))
+                ->add('approve', 'submit', array('label'  => 'Zustimmung', 'attr'=> array('class'=>'small success')))
+                ->add('neutral', 'submit', array('label'  => 'Neutral', 'attr'=> array('class'=>'small')))
+                ->add('disapprove', 'submit', array('label'  => 'Ablehnung', 'attr'=> array('class'=>'small alert')))
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            // set it to the right question given by the form
+            $answer->setQuestion($em->getReference('MadwaysKommunalomatBundle:Question', $form->get('question')->getData() ));
+
+            // set the answer by clicked button
+            $answer->setAnswerAsString($form->getClickedButton()->getName());
+
+            $em->persist($user);
+            $em->flush();
+            $answer->setUser($user);
+            $em->persist($answer);
+            $em->flush();
+
+            if($last_question) {
+                return $this->redirect($this->generateUrl('static_pages'));
+            }
+
+            // more or less dirty work around to reset the form and display the next question
+            return $this->forward('MadwaysKommunalomatBundle:Question:Index', array('weight' => $weight, 'request' => new Request()));
         }
 
         return array('question' => $question,
+                     'form' => $form->createView(),
                      'question_count' => $this->_getQuestionCount());
     }
 
