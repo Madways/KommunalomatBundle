06ceaaeeb6d7a5889c88a5fc4efbd409ee98aff8
morbidick <lukas.mohrbacher@web.de>
2014-04-27 18:23:54 +0200

    Party Controller

---
+++ Controller/PartyController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Madways\KommunalomatBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Madways\KommunalomatBundle\Entity\Party;
+use Madways\KommunalomatBundle\Entity\Question;
+use Madways\KommunalomatBundle\Entity\PartyAnswer;
+use Madways\KommunalomatBundle\Form\Type\PartyAnswerType;
+
+class PartyController extends Controller
+{
+    /**
+    * @Template()
+    */
+    public function indexAction() 
+    {
+
+        // Initiate Repository
+        $repository = $this->getDoctrine()->getRepository('MadwaysKommunalomatBundle:Party');
+
+        // Find the question by weight
+        $parties = $repository->findAll();
+
+        if (!$parties) {
+            throw $this->createNotFoundException('No Parties found!'); //TODO: handle this more graceful, maybe redirect to create page?
+        }
+
+        return array('parties' => $parties);
+    }
+
+    /**
+    * @Template()
+    */
+    public function formAction(Request $request, $id = NULL)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if (isset($id)) {
+            $party = $em->find('MadwaysKommunalomatBundle:Party', $id);
+
+            if (!$party) {
+                throw new NotFoundHttpException("Invalid party.");
+            }
+        } else {
+            $party = new Party();
+        }
+
+        $form = $this->createFormBuilder($party)
+                ->add('name', 'text')
+                ->add('description')
+                ->add('save', 'submit')
+                ->getForm();
+
+        // TODO: make it possible to upload a picture
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($party);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl("MadwaysKommunalomatBundleParty"));
+        }
+
+        return array('form' => $form->createView());
+    }
+
+    /**
+    * 
+    * @Template()
+    */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $party = $em->find('MadwaysKommunalomatBundle:Party', $id);
+
+        if (!$party) {
+            throw new NotFoundHttpException("Invalid Party.");
+        }
+
+        $em->remove($party);
+        $em->flush();
+
+        // TODO: ask for confirmation
+
+        return $this->redirect($this->generateUrl('MadwaysKommunalomatBundleParty'));
+    }
+
+    /**
+    * @Template()
+    */
+    public function answerAction($id, $weight, Request $request) 
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $party = $em->find('MadwaysKommunalomatBundle:Party', $id);
+
+        if (!$party) {
+            throw new NotFoundHttpException("Invalid Party.");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        // Find the question by weight
+        $question = $em->getRepository('MadwaysKommunalomatBundle:Question')->findOneByWeight($weight);
+
+        if (!$question) {
+            throw new NotFoundHttpException("Invalid Question.");
+        }
+
+        // Find already given answer
+        $answer = $em->getRepository('MadwaysKommunalomatBundle:PartyAnswer')->findOneBy(array('party' => $party, 'question' => $question));
+        if (!$answer) {
+            $answer = new PartyAnswer();
+        }
+
+        $form = $this->createFormBuilder($answer)
+                ->add('question', 'hidden', array('mapped' => false, 'data' => $question->getID() ))
+                ->add('explanation', 'text')
+                ->add('answer', 'choice', 
+                    array('choices' => array('0' => 'approve', 
+                                            '1' => 'neutral', 
+                                            '2' => 'disapprove'),
+                            'multiple' => false,
+                            'expanded' => true ))
+                ->add('submit', 'submit')
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $answer->setQuestion($em->getReference('MadwaysKommunalomatBundle:Question', $form->get('question')->getData() ));
+
+            // set user by id
+            $answer->setParty($party);
+
+            $em->persist($answer);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('MadwaysKommunalomatBundlePartyAnswer', array('id' => $party->getId(), 'weight' => $weight+1 )));
+        }
+
+        return array('party' => $party,
+                     'question' => $question,
+                     'form' => $form->createView(),
+                     'question_count' => $this->_getQuestionCount());
+        
+    }
+
+    /*
+    * Helper function to get the count of all questions
+    */
+    private function _getQuestionCount() {
+        // Initiate Repository
+        $repository = $this->getDoctrine()
+                ->getRepository('MadwaysKommunalomatBundle:Question');
+        // get count of all questions
+        $question_count = $repository->createQueryBuilder("q")
+                                        ->select('count(q.id)')
+                                        ->getQuery()->getSingleScalarResult();
+        return $question_count;
+    }
+}
